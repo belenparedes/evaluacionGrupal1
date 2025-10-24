1ed722da94b009b07195c051dc18c97e4c07890c
beckerdelsal-pixel <beckerdelsal@gmail.com>
2025-10-23 19:42:28 -0500

    Pruebas de la culminacion de la seccion de Transacciones, funcionamiento correcto

--- transacciones.js
+++ transacciones.js
@@ -15,10 +15,43 @@ cargar=function(){
     si existe retorna el objeto cuenta, caso contrario retorna null. 
 */
 buscarCuenta=function(numeroCuenta){
+    let cuentaEncontrada = null;
+    let Numcuenta;
+    for (let i = 0; i < cuentas.length; i++) {
+        Numcuenta = cuentas[i];
+        if (Numcuenta.numeroCuenta == numeroCuenta) {
+            cuentaEncontrada = Numcuenta;
+            break;
+        }else{
+            cuentaEncontrada = null;
+        }
+    }
+    return cuentaEncontrada;
 
 }
 
 ejecutarBusqueda=function(){
+    let valor;
+    let cuentaHallada;
+    valor = recuperarTexto("txtCuenta");
+    cuentaHallada = buscarCuenta(valor);
+    if (cuentaHallada === null) {
+        mostrarTexto("lblError", "CUENTA INVALIDA");
+        mostrarTextoEnCaja("txtCuenta","");
+        mostrarTexto("lblCedulaT", "");
+        mostrarTexto("lblNombreT", "");
+        mostrarTexto("lblApellidoT", "");
+        mostrarTexto("lblNumeroCuentaT", "");
+        mostrarTexto("lblSaldoT", "");
+    }else{
+        mostrarTexto("lblError","");
+        mostrarTexto("lblNumeroCuentaT", cuentaHallada.numeroCuenta);
+        mostrarTexto("lblCedulaT", cuentaHallada.cedula);
+        mostrarTexto("lblNombreT", cuentaHallada.nombre);
+        mostrarTexto("lblApellidoT", cuentaHallada.apellido);
+
+        mostrarTexto("lblSaldoT", cuentaHallada.saldo);
+    }
     //toma el numero de cuenta de la caja de texto
     //invoca a buscarCuenta y guarda el resultado en una variable
     //Si el resultado es diferente de null, muestra en pantalla, caso contrario muestra un alert
@@ -26,29 +59,54 @@ ejecutarBusqueda=function(){
 
 depositar=function(numeroCuenta,monto){
     let cuentaAfectada;
+    cuentaAfectada = buscarCuenta(numeroCuenta);
+    cuentaAfectada.saldo+=monto;
+    return cuentaAfectada.saldo;
     //invoca a buscarCuenta, guarda el resultado en la variable cuentaAfectada;
     //Al saldo actual de la cuenta afectada, le suma el monto que recibe como parámetro
 }
 
 ejecutarDeposito=function(){
+    let numCuenta;
+    let montoIngresado;
+    let nuevoSaldo;
+    numCuenta = recuperarTexto("txtCuenta");
     //Toma el numero de cuenta ingresado en la caja de texto
+    montoIngresado = recuperarFloat("txtMonto");
     //Toma el monto ingresado en la caja de texto
+    nuevoSaldo = depositar(numCuenta,montoIngresado);
     //invoca a depositar
+    alert("TRANSACCION EXITOSA");
     //Muestra un mensaje TRANSACCION EXITOSA
+    mostrarTexto("lblSaldoT",nuevoSaldo);
     //Muestra en pantalla el nuevo saldo de la cuenta
-}
-
-depositar=function(numeroCuenta,monto){
-    let cuentaAfectada;
-    //invoca a buscarCuenta, guarda el resultado en la variable cuentaAfectada;
-    //Al saldo actual de la cuenta afectada, le suma el monto que recibe como parámetro
 }
 
 retirar=function(numeroCuenta,monto){
     let cuentaAfectada;
+    cuentaAfectada = buscarCuenta(numeroCuenta);
     //invoca a buscarCuenta, guarda el resultado en la variable cuentaAfectada;
+    if (cuentaAfectada.saldo < monto){
+        alert("SALDO INSUFICIENTE");
+    }else if(cuentaAfectada.saldo>=monto){
+        cuentaAfectada.saldo -= monto;
+        alert("TRANSACCION EXITOSA");
+    }
+    return cuentaAfectada.saldo;
+    //Al saldo actual de la cuenta afectada, le suma el monto que recibe como parámetro
+}
+
+ejecutarRetirar=function(){
+    let numCuenta;
+    let montoIngresado;
+    let nuevoSaldo;
+    numCuenta = recuperarTexto("txtCuenta");
+    //invoca a buscarCuenta, guarda el resultado en la variable cuentaAfectada;
+    montoIngresado = recuperarFloat("txtMonto");
     //Valida si la cuenta tiene el saldo suficiente para retirar el monto
+    nuevoSaldo = retirar(numCuenta,montoIngresado);
     //Si el saldo es suficiente,al saldo actual de la cuenta afectada, le resta el monto que recibe como parámetro
     //Si el saldo no es suficiente, muestra un alert SALDO INSUFICIENTE
+    mostrarTexto("lblSaldoT",nuevoSaldo);
     //Si logra retirar muestra un mensaje TRANSACCION EXITOSA y muestra en pantalla el nuevo saldo de la cuenta
 }
